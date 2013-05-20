824414424ea6b11d0ebe28ec3745237976b3283e
Luciano Leggieri <lleggieri@despegar.com>
2013-05-20 15:41:41 -0300

    Version with traits instead of inheritance

--- src/main/scala/com/lucho/N.scala
+++ src/main/scala/com/lucho/N.scala
@@ -1,108 +1,133 @@
 package com.lucho
 
-private[lucho] class N(value: Long) {
-
-  private def self = value
+/**
+ * Class N from Number.
+ * @param v the long number it represents.
+ */
+private[lucho] class N(val v: Long) extends Ordered[N] {
 
   override def equals(that: Any) = that match {
-    case z: Z => value == 0
-    case n: N => value == n.self
-    case l: Long => value == l
-    case i: Int => value == i
+    case n: N => v == n.v
     case _ => false
   }
 
-  override def hashCode() = value.toInt
+  override def hashCode() = if (v >= Int.MinValue && v <= Int.MaxValue) v.toInt else v.## //copied from ScalaNumericConversions
 
-  def hundred(n: N): N = if (value < 100) new N(value * 100 + n.self) else new N((value / 100) * 100 + (value % 100) * 100 + n.self)
-  def hundred(t: T): T = if (value < 100) new T(value * 100 + t.self) else new T((value / 100) * 100 + (value % 100) * 100 + t.self)
+  override def toString = v.toString
 
-  def hundred(thou: Thou): N = new N((this hundred) * 1000)
+  override def compare(that: N): Int = v.compareTo(that.v)
 
-  def hundred: N = if (value < 100) new N(value * 100) else new N((value / 100) * 100 + (value % 100) * 100)
-
-  def thousand(n: N): N = if (value < 1000) new N(value * 1000 + n.self) else new N((value / 1000) * 1000 + (value % 1000) * 1000 + n.self)
-  def thousand(t: T): T = if (value < 1000) new T(value * 1000 + t.self) else new T((value / 1000) * 1000 + (value % 1000) * 1000 + t.self)
-
-  def thousand: N = if (value < 1000) new N(value * 1000) else new N((value / 1000) * 1000 + (value % 1000) * 1000)
-
-  def million(n: N): N = new N(value * 1000000 + n)
-  def million(t: T): N = new N(value * 1000000 + t)
-
-  override def toString = value.toString
-}
-
-private[lucho] class T(value: Long) extends N(value) {
-
-  def one = new N(value + 1)
-  def two = new N(value + 2)
-  def three = new N(value + 3)
-  def four = new N(value + 4)
-  def five = new N(value + 5)
-  def six = new N(value + 6)
-  def seven = new N(value + 7)
-  def eight = new N(value + 8)
-  def nine = new N(value + 9)
+  def + (that: N): N with F = new N(v + that.v) with F
+  def - (that: N): N with F = new N(v - that.v) with F
+  def * (that: N): N with F = new N(v * that.v) with F
+  def / (that: N): N with F = new N(v / that.v) with F
+  def % (that: N): N with F = new N(v % that.v) with F
 
 }
 
-private[lucho] class Z {
+/**
+ * HTM from Hundred, Thousand, Million. Adds these extensions to a Number.
+ */
+private[lucho] trait HTM { n: N =>
 
-  override def toString = "0"
+  def hundred(t: T): N with T = if (v < 100) new N(v * 100 + toValue(t)) with T else new N((v / 100) * 100 + (v % 100) * 100 + toValue(t)) with T
+  def hundred(e: E): N with E = if (v < 100) new N(v * 100 + toValue(e)) with E else new N((v / 100) * 100 + (v % 100) * 100 + toValue(e)) with E
 
-  override def equals(that: Any) = that match {
-    case z: Z => true
-    case n: N => n == 0
-    case l: Long => l == 0
-    case i: Int => i == 0
-    case _ => false
-  }
+  def hundred(aux: Auxi): N with E = new N((this hundred) * aux.multiplier) with E
 
-  override def hashCode() = 0
+  def hundred: N with E = if (v < 100) new N(v * 100) with E else new N((v / 100) * 100 + (v % 100) * 100) with E
+
+  def thousand(t: T): N with T = if (v < 1000) new N(v * 1000 + toValue(t)) with T else new N((v / 1000) * 1000 + (v % 1000) * 1000 + toValue(t)) with T
+  def thousand(e: E): N with E = if (v < 1000) new N(v * 1000 + toValue(e)) with E else new N((v / 1000) * 1000 + (v % 1000) * 1000 + toValue(e)) with E
+
+  def thousand: N with E = if (v < 1000) new N(v * 1000) with E else new N((v / 1000) * 1000 + (v % 1000) * 1000) with E
+
+  def million(t: T): N with T = new N(v * 1000000 + toValue(t)) with T
+  def million(e: E): N with E = new N(v * 1000000 + toValue(e)) with E
+
+  def million: N with E = new N(v * 1000000) with E
+
+  private def toValue(t: T): Long = N.toLong(t)
+  private def toValue(e: E): Long = N.toLong(e)
 
 }
 
-private[lucho] class Thou {}
+/**
+ * F from Final. These Numbers cannot be used with thousand, hundred or million.
+ */
+private[lucho] trait F { n: N =>
+
+}
+
+/**
+ * E from Extensible. These Numbers can be used with thousand, hundred or million.
+ */
+private[lucho] trait E extends HTM { n: N =>
+
+}
+
+/**
+ * T from Tenth. These Numbers have methods to build Numbers from 21 to 99.
+ */
+private[lucho] trait T extends HTM { n: N =>
+
+  def one: N = n + N.one
+  def two: N = n + N.two
+  def three: N = n + N.three
+  def four: N = n + N.four
+  def five: N = n + N.five
+  def six: N = n + N.six
+  def seven: N = n + N.seven
+  def eight: N = n + N.eight
+  def nine: N = n + N.nine
+
+}
+
+private[lucho] abstract sealed class Auxi(private[lucho] val multiplier: Long) {}
+private[lucho] class Thou extends Auxi(1000) {}
+private[lucho] class Mill extends Auxi(1000000) {}
 
 object N {
 
-  @inline implicit def toLong(n: N): Long = n.self
-  @inline implicit def toLong(Z: Z): Long = 0
+  @inline implicit def toLong(e: E): Long = e.asInstanceOf[N].v
+  @inline implicit def toLong(t: T): Long = t.asInstanceOf[N].v
+  @inline implicit def toLong(f: F): Long = f.asInstanceOf[N].v
 
-  @inline implicit def toN(l: Long) = new N(l)
+  //@inline implicit def toN(l: Long) = new N(l) with F
 
-  val zero = new Z
+  val zero = new N(0) with F
 
-  val one = new N(1)
-  val two = new N(2)
-  val three = new N(3)
-  val four = new N(4)
-  val five = new N(5)
-  val six = new N(6)
-  val seven = new N(7)
-  val eight = new N(8)
-  val nine = new N(9)
+  val one = new N(1) with E
+  val two = new N(2) with E
+  val three = new N(3) with E
+  val four = new N(4) with E
+  val five = new N(5) with E
+  val six = new N(6) with E
+  val seven = new N(7) with E
+  val eight = new N(8) with E
+  val nine = new N(9) with E
 
-  val ten = new N(10)
-  val eleven = new N(11)
-  val twelve = new N(12)
-  val thirteen = new N(13)
-  val fourteen = new N(14)
-  val fifteen = new N(15)
-  val sixteen = new N(16)
-  val seventeen = new N(17)
-  val eighteen = new N(18)
-  val nineteen = new N(19)
+  val ten = new N(10) with E
+  val eleven = new N(11) with E
+  val twelve = new N(12) with E
+  val thirteen = new N(13) with E
+  val fourteen = new N(14) with E
+  val fifteen = new N(15) with E
+  val sixteen = new N(16) with E
+  val seventeen = new N(17) with E
+  val eighteen = new N(18) with E
+  val nineteen = new N(19) with E
 
-  val twenty = new T(20)
-  val thirty = new T(30)
-  val forty = new T(40)
-  val fifty = new T(50)
-  val sixty = new T(60)
-  val seventy = new T(70)
-  val eighty = new T(80)
-  val ninety = new T(90)
+  val twenty = new N(20) with T
+  val thirty = new N(30) with T
+  val forty = new N(40) with T
+  val fifty = new N(50) with T
+  val sixty = new N(60) with T
+  val seventy = new N(70) with T
+  val eighty = new N(80) with T
+  val ninety = new N(90) with T
 
   val thousand = new Thou
+  val million = new Mill
 
 }
--- src/test/scala/TestN.scala
+++ src/test/scala/TestN.scala
@@ -4,30 +4,32 @@ import com.lucho.N._
 class TestN extends FunSuite {
 
   test("sum") {
-    assert(one + 1 == 2)
-    assert(1 + one == 2)
+    assert(one.v + 1 === 2)
+    assert(1 + one.v === 2)
+    assert(one + one === two)
   }
 
   test("equality") {
-    assert(one == 1)
+    assert(one.v == 1)
+    assert(one.v === 1)
     //assert((1 == one)) //cannot override ==
-    assert((twenty three) === 23)
-    assert((one hundred) === 100)
-    assert((one hundred three) === 103)
-    assert((one hundred twenty) === 120)
-    assert((one hundred twenty three) === 123)
+    assert((twenty three).v === 23)
+    assert((one hundred).v === 100)
+    assert((one hundred three).v === 103)
+    assert((one hundred twenty).v === 120)
+    assert((one hundred twenty three).v === 123)
 
-    assert((two thousand) === 2000)
-    assert((two thousand five) === 2005)
-    assert((two thousand twenty) === 2020)
-    assert((two thousand twenty three) === 2023)
-    assert((two thousand one hundred) === 2100)
-    assert((two thousand one hundred five) === 2105)
-    assert((two thousand one hundred twenty) === 2120)
-    assert((two thousand one hundred twenty three) === 2123)
-    assert((three million (one hundred twenty three)) === 3000123)
-    assert((three million four hundred eleven thousand one hundred twenty three) === 3411123)
-    assert(zero === 0)
+    assert((two thousand).v === 2000)
+    assert((two thousand five).v === 2005)
+    assert((two thousand twenty).v === 2020)
+    assert((two thousand twenty three).v === 2023)
+    assert((two thousand one hundred).v === 2100)
+    assert((two thousand one hundred five).v === 2105)
+    assert((two thousand one hundred twenty).v === 2120)
+    assert((two thousand one hundred twenty three).v === 2123)
+    assert((three million one hundred twenty three).v === 3000123)
+    assert((three million four hundred eleven thousand one hundred twenty three).v === 3411123)
+    assert(zero.v === 0)
   }
 
   test("equality 2") {
@@ -36,8 +38,9 @@ class TestN extends FunSuite {
   }
 
   test("minus") {
-    assert((two - two) == 0)
-    assert((two - two) === 0)
+    assert((two - two).v == 0)
+    assert((two - two).v === 0)
+    assert((two - two) === zero)
 
     assert(zero == (two - two))
     assert(zero === (two - two))
@@ -46,7 +49,22 @@ class TestN extends FunSuite {
   test("outliers") {
     //val x1 = (one hundred hundred) cannot do it, ok
     //val x2 = (one thousand hundred) cannot do it, ok
-    assert((one hundred thousand) === 100000)
+    assert((one hundred thousand).v === 100000)
+    assert((one hundred million).v === 100000000)
+    //assert((one hundred zero).v === 100000000) cannot do it, ok
+    //assert(((two + four) hundred) === (six hundred)) cannot do it, ok
+  }
+
+  test("implicit") {
+    assert(one.toLong == 1)
+  }
+
+  test("sort") {
+    assert(one < two)
+    assert(four > three)
+    val unsorted = List(three, one, two)
+    val expected = List(one, two, three)
+    //assert(unsorted.sorted === expected)
   }
 
 }
